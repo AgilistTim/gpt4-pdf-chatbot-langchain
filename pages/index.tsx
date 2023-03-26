adf4f5f21dcefec1ebbb2d6b5cd35b878fe32b4d
AgilistTim <122605124+AgilistTim@users.noreply.github.com>
2023-03-26 11:26:23 +0100

    prompt tweak

--- pages/index.tsx
+++ pages/index.tsx
@@ -317,7 +317,7 @@ export default function Home() {
         </div>
         <footer className="m-auto p-4">
           <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+            Powered by LangChainAI. modified from a Demo built by Mayo (Twitter: @mayowaoshin).
           </a>
         </footer>
       </Layout>
