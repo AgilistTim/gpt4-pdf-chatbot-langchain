3e4e58ee5070acb548b40db8b7c2321b21134fcc
AgilistTim <122605124+AgilistTim@users.noreply.github.com>
2023-04-18 18:35:13 +0100

    safe bot

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -2,8 +2,8 @@
  * Change the index and namespace to your own
  */
 
-const PINECONE_INDEX_NAME = 'cockburn';
+const PINECONE_INDEX_NAME = 'safe';
 
-const PINECONE_NAME_SPACE = 'ask_alistair'; //namespace is optional for your vectors
+const PINECONE_NAME_SPACE = 'safe bot'; //namespace is optional for your vectors
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
